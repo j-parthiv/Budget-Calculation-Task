9683ff3b2efdbbc25902c328e788a173dace7eb9
j-parthiv <parthivjani9@gmail.com>
2024-09-23 12:50:57 +0200

    feat: add toast in the root element

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
+import { Toaster } from "@/components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Toaster />
     </ThemeProvider>
   </StrictMode>
 );
