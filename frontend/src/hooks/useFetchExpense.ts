da436df0799eab6b4223f356a25138f28c5fadfa
j-parthiv <parthivjani9@gmail.com>
2024-09-23 12:49:10 +0200

    feat: create a hook to fetch the expenses

---
+++ frontend/src/hooks/useFetchExpense.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Expense } from '../types';
+
+const useFetchExpenses = () => {
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await axios.get<Expense[]>(`${import.meta.env.VITE_API_BASE_URL}/Expenses`);
+        setExpenses(response.data);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'An unknown error occurred');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchExpenses();
+  }, []);
+
+  return { expenses, loading, error };
+};
+
+export default useFetchExpenses;
