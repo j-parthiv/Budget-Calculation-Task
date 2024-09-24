f0c2a9285563c321435b822fae509b4ba9f94c44
j-parthiv <parthivjani9@gmail.com>
2024-09-24 18:30:21 +0200

    feat: add validations on the model

--- backend/CinemaCalcBackend/Models/Expense.cs
+++ backend/CinemaCalcBackend/Models/Expense.cs
@@ -1,4 +1,6 @@
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CinemaCalcBackend.Models
 {
@@ -7,15 +9,21 @@ namespace CinemaCalcBackend.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters.")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Price is required.")]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0, 1000000000, ErrorMessage = "Price must be between 0 and 1,000,000,000.")]
         public decimal Price { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "PercentageMarkup is required.")]
+        [Column(TypeName = "decimal(5,2)")]
+        [Range(0, 100, ErrorMessage = "PercentageMarkup must be between 0 and 100.")]
         public decimal PercentageMarkup { get; set; }
 
-        public decimal TotalPrice => Price + (Price * (PercentageMarkup / 100));
+        [NotMapped]
+        public decimal TotalPrice => Math.Round(Price + (Price * PercentageMarkup / 100), 2, MidpointRounding.AwayFromZero);
     }
 }
