c5120d809524411430b13f51bd0ecb3f6a8b3603
j-parthiv <parthivjani9@gmail.com>
2024-09-24 18:33:21 +0200

    feat: create a custom hook to delete expense

---
+++ frontend/src/hooks/useDeleteExpense.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@/hooks/use-toast";
+
+const useDeleteExpense = () => {
+  const { toast } = useToast();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  const deleteExpense = async (id: number) => {
+    setLoading(true);
+    setError(null);
+    try {
+      await axios.delete(`${import.meta.env.VITE_API_BASE_URL}/Expenses/${id}`);
+      toast({
+        title: "✅ Success",
+        description: "Expense deleted successfully!",
+      });
+    } catch (err: any) {
+      setError(err.message);
+      toast({
+        variant: "destructive",
+        title: "❕ Error",
+        description: err.message || "An error occurred",
+      });
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { deleteExpense, loading, error };
+};
+
+export default useDeleteExpense;
