3b9b455ba91bee248cf09f6b56422903a6d77cd5
j-parthiv <parthivjani9@gmail.com>
2024-09-25 14:04:15 +0200

    feat: add docker configuration and compose file

--- backend/CinemaCalcBackend/Controllers/ExpensesController.cs
+++ backend/CinemaCalcBackend/Controllers/ExpensesController.cs
@@ -2,10 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CinemaCalcBackend.Data;
 using CinemaCalcBackend.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CinemaCalcBackend.Controllers
 {
